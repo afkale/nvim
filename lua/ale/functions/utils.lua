285881523218b2dbdf05e5bb9a6920120f5948b3
Alejandro Rubio Oliva <a.rubio@Alejandros-MacBook-Pro.local>
2024-06-27 20:55:16 +0200

    feat: new functionalities
    > cover strings with some symbols
    > move lines horizontaly with TAB

--- lua/ale/functions/utils.lua
+++ lua/ale/functions/utils.lua
@@ -6,6 +6,16 @@ local get_os = function()
 	return vim.loop.os_uname().sysname
 end
 
+local couples = {
+	["'"] = "'",
+	['"'] = '"',
+	["`"] = "`",
+	["{"] = "}",
+	["["] = "]",
+	["("] = ")",
+	["<"] = ">",
+}
+
 return {
 	get_os = get_os,
 	get_home = function()
@@ -28,4 +38,19 @@ return {
 		end
 		return res
 	end,
+	get_visual_selection_indices = function()
+		local c_buf, s_row, s_col, _ = unpack(vim.fn.getpos("'<"))
+		local _, e_row, e_col, _ = unpack(vim.fn.getpos("'>"))
+
+		s_row = s_row - 1
+		s_col = s_col - 1
+		e_row = e_row - 1
+		e_col = e_col - 1
+
+		return c_buf, s_row, s_col, e_row, e_col
+	end,
+	get_couple = function(key)
+		return key, couples[key]
+	end,
+	couples = couples,
 }
--- lua/ale/remap.lua
+++ lua/ale/remap.lua
@@ -1,3 +1,5 @@
+local utils = require("ale.functions.utils")
+
 vim.g.mapleader = " "
 
 vim.keymap.set({ "v", "x" }, "<leader>y", '"+y')
@@ -18,7 +20,6 @@ vim.keymap.set("n", "<leader><leader>", ":source<cr>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
@@ -50,29 +51,49 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+function SurroundSelectionWithPair(char)
+	if vim.fn.visualmode() ~= "v" then
+		return
+	end
+
+	local c_buf, s_row, s_col, e_row, e_col = utils.get_visual_selection_indices()
+	local text = vim.api.nvim_buf_get_text(c_buf, s_row, s_col, e_row, e_col + 1, {})
+
+	local s_char, e_char = utils.get_couple(char)
+
+	text[1] = s_char .. text[1]
+	text[#text] = text[#text] .. e_char
+
+	vim.api.nvim_buf_set_text(c_buf, s_row, s_col, e_row, e_col + 1, text)
+	return text
+end
+
+for key, _ in pairs(utils.couples) do
+	local command = string.format(':lua SurroundSelectionWithPair("%s")<CR>', key)
+	vim.keymap.set("v", key, command, { noremap = true, silent = true })
+end
+
+vim.keymap.set({ "v", "n" }, "<Tab>", ">>", { noremap = true, silent = true })
+vim.keymap.set({ "v", "n" }, "<S-Tab>", "<<", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
 
 -- LSP CMDS
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-vim.api.nvim_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-vim.api.nvim_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>wl",
-	"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-	opts
-)
-vim.api.nvim_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Format<CR>", opts)
+vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.keymap.set("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+vim.keymap.set("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+vim.keymap.set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+vim.keymap.set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+vim.keymap.set("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+vim.keymap.set("n", "<leader>ff", "<cmd>Format<CR>", opts)
