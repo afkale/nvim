b5ca30c66b0ff179925503f8773374168cc5ba98
ale <arubioo977@gmail.com>
2025-02-09 18:49:43 +0100

    feat: add comments to files extra plugins

--- lua/ale/extra/files.lua
+++ lua/ale/extra/files.lua
@@ -1,25 +1,29 @@
+-- Toggle minifiles function.
 local function toggle_minifiles(opts)
-	opts = opts or {}
 	if vim.bo.filetype == "minifiles" then
+		-- If minifiles is already open close it.
 		MiniFiles.close()
 	else
+		-- If not open it.
 		MiniFiles.open(opts.path)
 	end
 end
 
+-- User command to open minifiles in current file directory.
 vim.api.nvim_create_user_command("FilesHere", function()
 	toggle_minifiles({ path = vim.fn.expand("%:p") })
 end, {})
 
-vim.api.nvim_create_user_command("Files", function()
-	toggle_minifiles()
-end, {})
+-- User command to open minifiles.
+vim.api.nvim_create_user_command("Files", toggle_minifiles, {})
 
--- Sync MiniFiles with CR
+-- Sync minifiles with CR on minifiles filetype.
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "minifiles" },
 	callback = function(args)
-		vim.api.nvim_buf_set_keymap(args.buf, "n", "<CR>", ":lua MiniFiles.synchronize()<CR>",
-			{ noremap = true, silent = true })
+		-- Add buf keymap to synchronize with enter keymap only on minifiles buffers.
+		vim.api.nvim_set_keymap(
+			"n", "<CR>", ":lua MiniFiles.synchronize()<CR>", { noremap = true, silent = true }
+		)
 	end
 })
