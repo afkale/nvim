bf535f584b60850d39b075248c2ea34a609dafa5
Alejandro Rubio Oliva <alejandro-rubiooliva@alejandro.rubiooliva>
2025-08-19 17:54:25 +0200

    (feat): moving word switcher into a plugin

--- lua/ale/extra/utils.lua
+++ lua/ale/extra/utils.lua
@@ -1,5 +1,29 @@
 local utils = {}
 
+local WordSwitch = {}
+local H = {}
+
+WordSwitch.config = {}
+
+WordSwitch.setup = function(config)
+  _G.WordSwitch = WordSwitch
+  config = H.setup_config(config)
+end
+
+H.default_config = vim.deepcopy(WordSwitch.config)
+
+H.check_type = function(name, val, ref, allow_nil)
+  if type(val) == ref or (ref == 'callable' and vim.is_callable(val)) or (allow_nil and val == nil) then return end
+  H.error(string.format('`%s` should be %s, not %s', name, ref, type(val)))
+end
+
+H.setup_config = function(config)
+  H.check_type('config', config, 'table', true)
+  config = vim.tbl_deep_extend('force', vim.deepcopy(H.default_config), config or {})
+
+  return config
+end
+
 utils.switch = function()
   local map = {
     True = "False",
