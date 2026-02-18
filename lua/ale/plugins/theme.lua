4a3120ee602bc9c2d74d6f775dbbc95777844ceb
Alejandro Rubio Oliva <alejandro-rubiooliva@alejandro.rubiooliva>
2026-02-18 08:48:17 +0100

    (feat): fixed settings

--- lua/ale/plugins/theme.lua
+++ lua/ale/plugins/theme.lua
@@ -4,7 +4,8 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        styles = { transparency = true}
+        dim_inactive_windows = true,
+        styles = { transparency = true }
       })
 
       vim.cmd("colorscheme rose-pine")
--- lua/ale/set.lua
+++ lua/ale/set.lua
@@ -38,19 +38,19 @@ vim.opt.hlsearch = false  -- Don't highlight search results
 vim.opt.incsearch = true  -- Show matches as you type
 
 -- Visual settings
-vim.opt.termguicolors = true                      -- Enable 24-bit colors
-vim.opt.signcolumn = 'yes'                        -- Always show sign column
-vim.opt.colorcolumn = '100'                       -- Show column at 100 characters
-vim.opt.showmatch = true                          -- Highlight matching brackets
-vim.opt.matchtime = 2                             -- How long to show matching bracket
-vim.opt.cmdheight = 1                             -- Command line height
+vim.opt.termguicolors = true -- Enable 24-bit colors
+vim.opt.signcolumn = 'yes'   -- Always show sign column
+vim.opt.colorcolumn = '100'  -- Show column at 100 characters
+vim.opt.showmatch = true     -- Highlight matching brackets
+vim.opt.matchtime = 2        -- How long to show matching bracket
+vim.opt.cmdheight = 1        -- Command line height
 vim.opt.completeopt = 'menuone,popup,noinsert'
-vim.opt.showmode = false                          -- Don't show mode in command line
-vim.opt.pumheight = 10                            -- Popup menu height
-vim.opt.conceallevel = 0                          -- Don't hide markup
-vim.opt.concealcursor = ''                        -- Don't hide cursor line markup
-vim.opt.lazyredraw = true                         -- Don't redraw during macros
-vim.opt.synmaxcol = 300                           -- Syntax highlighting limit
+vim.opt.showmode = false     -- Don't show mode in command line
+vim.opt.pumheight = 10       -- Popup menu height
+vim.opt.conceallevel = 0     -- Don't hide markup
+vim.opt.concealcursor = ''   -- Don't hide cursor line markup
+vim.opt.lazyredraw = true    -- Don't redraw during macros
+vim.opt.synmaxcol = 300      -- Syntax highlighting limit
 
 -- File handling
 vim.opt.backup = false                            -- Don't create backup files
