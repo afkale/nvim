fbf4e8f3ad29de452b9b9db0e242183a4f9eedd2
afkale <arubioo977@gmail.com>
2026-05-24 13:09:14 +0200

    (feat): remove copilot dependency

--- lua/ale/plugins/init.lua
+++ lua/ale/plugins/init.lua
@@ -10,13 +10,4 @@ return {
       vim.keymap.set("n", "<leader>cc", quicker.toggle, { desc = "Toggle quickfix" })
     end
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {
-    },
-  },
 }
