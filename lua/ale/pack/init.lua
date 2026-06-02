fe0ce133e0cf4eaa0f5d91745beedec1720829b9
Alejandro Rubio Oliva <alejandro-rubiooliva@alejandro.rubiooliva>
2026-06-02 13:19:09 +0200

    (feat): indentation and notifications

--- lua/ale/pack/init.lua
+++ lua/ale/pack/init.lua
@@ -1,22 +1,24 @@
 vim.pack.add({
-  { src = "https://github.com/rose-pine/neovim",                name = "rose-pine" },
-  { src = "https://github.com/nvim-lualine/lualine.nvim",       name = "lualine" },
+  { src = "https://github.com/rose-pine/neovim",                        name = "rose-pine" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim",               name = "lualine" },
 
-  { src = "https://github.com/lewis6991/gitsigns.nvim",         name = "gitsigns" },
-  { src = "https://github.com/afkale/wswitcher.nvim",           name = "wswitcher" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim",                 name = "gitsigns" },
+  { src = "https://github.com/afkale/wswitcher.nvim",                   name = "wswitcher" },
 
-  { src = "https://github.com/nvim-mini/mini.nvim",             name = "mini.nvim" },
+  { src = "https://github.com/nvim-mini/mini.nvim",                     name = "mini.nvim" },
 
-  { src = "https://github.com/stevearc/quicker.nvim",           name = "quicker" },
-  { src = "https://github.com/neovim/nvim-lspconfig",           name = "nvim-lspconfig" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", name = "nvim-treesitter", branch = "main" },
+  { src = "https://github.com/stevearc/quicker.nvim",                   name = "quicker" },
+  { src = "https://github.com/neovim/nvim-lspconfig",                   name = "nvim-lspconfig" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter",         name = "nvim-treesitter",        branch = "main" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-context", name = "nvim-treesitter-context" },
 
-  { src = "https://github.com/mfussenegger/nvim-dap",           name = "nvim-dap" },
-  { src = "https://github.com/mfussenegger/nvim-dap-python",    name = "nvim-dap-python" },
-  { src = "https://github.com/igorlfs/nvim-dap-view",           name = "nvim-dap-view" },
-  { src = "https://github.com/mason-org/mason.nvim",            name = "nvim-mason" },
 
-  { src = "https://github.com/tpope/vim-fugitive",              name = "vim-fungitive" },
+  { src = "https://github.com/mfussenegger/nvim-dap",                   name = "nvim-dap" },
+  { src = "https://github.com/mfussenegger/nvim-dap-python",            name = "nvim-dap-python" },
+  { src = "https://github.com/igorlfs/nvim-dap-view",                   name = "nvim-dap-view" },
+  { src = "https://github.com/mason-org/mason.nvim",                    name = "nvim-mason" },
+
+  { src = "https://github.com/tpope/vim-fugitive",                      name = "vim-fungitive" },
 })
 
 require("ale.pack.theme")
--- lua/ale/pack/theme.lua
+++ lua/ale/pack/theme.lua
@@ -1,5 +1,6 @@
 require("rose-pine").setup({ dim_inactive_windows = true, styles = { transparency = true } })
 require("mini.icons").setup({})
+require("mini.notify").setup({})
 require("lualine").setup({})
 
 vim.cmd.colorscheme("rose-pine")
